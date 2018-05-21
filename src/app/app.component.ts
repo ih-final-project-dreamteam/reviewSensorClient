0b437b66d538d1da536d971570387819674a90ca
Dali Rojas <rojasdali@gmail.com>
2018-05-21 15:17:35 -0400

    tried to fix some stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private myService: AuthService) {}
   title = 'app';
+
+  formInfo: any = {username: '', password: ''};
+  user: any;
+  error: any;
+
+
+  login() {
+    this.myService.login(this.formInfo)
+    .subscribe(
+      (user) => this.user = user,
+      (err) => this.error = err
+    );
+  }
+  signup() {
+    this.myService.signup(this.formInfo)
+    .subscribe(
+      (user) => { this.user = user;
+      console.log(this.user);
+      },
+      (err) => this.error = err
+    );
+  }
+
+  logout() {
+    this.myService.logout()
+    .subscribe(
+      (user) => { this.user = null;
+      },
+      (err) => this.error = err
+    );
+  }
 }
+
