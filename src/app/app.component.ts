11e263a67e4978619aed37fa170162ff83dd9eeb
Eleyni Rodriguez <eleyni.m.rodriguez@gmail.com>
2018-05-22 17:34:56 -0400

    Add: Styles the top nav and landing page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor( private route: ActivatedRoute, private router: Router ) {}
+
+  onAnchorClick ( ) {
+    this.route.fragment.subscribe ( f => {
+      const element = document.querySelector ( "#" + f );
+      if (element) element.scrollIntoView (element)
+    });
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe(s => {
+      if (s instanceof NavigationEnd) {
+        const tree = this.router.parseUrl(this.router.url);
+        if (tree.fragment) {
+          const element = document.querySelector("#" + tree.fragment);
+          if (element) { element.scrollIntoView(element); }
+        }
+      }
+    });
+  }
+
+
 }
+
